c063e43f8596e401a49ab6fc02cb2ee74db8c87f
Deliefer879 <kgcaD@kgcaschool.com>
2023-07-18 18:00:30 +0900

    dd

--- Timer/main.cpp
+++ Timer/main.cpp
@@ -6,16 +6,12 @@ int main()
 {
 	Timer timer;
 	timer.Init();
-	float maxFrame = 30.0f;
-	float fstep = 1.0f / 15.0f;
-	float fStaticFrame = 0.0f;
 
 	while (timer.GetGameTimer() < std::chrono::seconds(30)) {
 		std::this_thread::sleep_for(milliseconds(1));
 		timer.Frame();
-		fStaticFrame = 0.0f;
 		//system("cls");
-		timer.Render();
+		//timer.Render();
 	}
 	timer.Release();
 }
